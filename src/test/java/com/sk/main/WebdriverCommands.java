c15bb877dbcfc6a6a9bed5410742967699aa904d
santosh9901 <tugay.santoshkumar@gmail.com>
2023-07-31 20:02:54 +0530

    added waits sample code

---
+++ src/test/java/com/sk/main/WebdriverCommands.java
@@ -0,0 +1,51 @@
+package com.sk.main;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebdriverCommands {
+
+	@Test
+	public void basicCommands() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get("https://artoftesting.com/sampleSiteForSelenium");
+		String text = driver.findElement(By.id("idOfDiv")).getText();
+		System.out.println(text);
+		Thread.sleep(5);
+
+		driver.findElement(By.linkText("This is a link")).click();
+		Thread.sleep(3000);
+
+		driver.findElement(By.id("fname")).sendKeys("Kuldeep Rana");
+		Thread.sleep(3000);
+
+		driver.findElement(By.id("fname")).clear();
+		Thread.sleep(3000);
+
+		driver.findElement(By.id("idOfButton")).click();
+		Thread.sleep(3000);
+
+		driver.findElement(By.name("male")).click();
+		Thread.sleep(3000);
+
+		driver.findElement(By.cssSelector("input.Automation")).click();
+		Thread.sleep(3000);
+
+		Select dropdown = new Select(driver.findElement(By.id("testingDropdown")));
+		dropdown.selectByVisibleText("Database Testing");
+
+		Thread.sleep(5000);
+		driver.close();
+
+	}
+}
